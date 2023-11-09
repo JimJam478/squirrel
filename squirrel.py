8019275c6e3c0748d7a69bc9d6dd3e9381953260
JimJam47 <allensreebinoj2010@gmail.com>
2023-11-09 21:16:23 +0530

    Implements compute phi function

--- squirrel.py
+++ squirrel.py
@@ -1,4 +1,5 @@
 import json
+import math
 
 def load_json():
     f = open('journal.json')
@@ -7,5 +8,27 @@ def load_json():
     f.close()
     return journal_list
 
+def compute_phi(file,event):
+    n_11,n_00,n_10,n_01 = 0  
+    n_1p,n_0p,n_p1,n_p0 = 0 
+    for i in file:
+        if event in i['events'] and i['squirrel']:
+            n_11 += 1
+        if event not in i['events'] and not i['squirrel']:
+            n_00 += 1
+        if event in i['events'] and not i['squirrel']:
+            n_10 += 1
+        if event not in i['events'] and i['squirrel']:
+            n_01 += 1
+        if event in i['events']:
+            n_1p += 1
+        if event not in i['events']:
+            n_0p += 1
+        if i['squirrel']:
+            n_p1 += 1
+        if not i['squirrel']:
+            n_p0 += 1
+    phi = (n_11 * n_00 - n_10 * n_01) / math.sqrt((n_1p * n_0p * n_p1 * n_p0))
 
+    
     
